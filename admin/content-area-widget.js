c5e565ba5df10e0a754ec9c8da8ca4e64e087c12
Zenobius Jiricek <airtonix@gmail.com>
2018-08-09 20:45:32 +0930

    Changess

--- admin/content-area-widget.js
+++ admin/content-area-widget.js
@@ -1,6 +1,6 @@
 var ContentAreaControl = createClass({
 
-    render: function (h) {
+    render: function () {
         return h('div', {
             class: 'content-area',
             onDrop: this.handleOnDrop,
@@ -13,7 +13,22 @@ var ContentAreaControl = createClass({
 });
 
 var ContentAreaPreview = createClass({
+    render: function () {
+        // build a list of block display elements
+        var blocks = this.props.value
+            .map(function (block, index) {
+                return h('div', {
+                    class: 'content-area__item',
+                }, block.name);
+            });
 
+        // wrap them in a container
+        return h('div', {
+            class: 'content-area content-area--preview'
+            },
+            blocks
+        );
+    }
 });
 
 CMS.registerWidget(
